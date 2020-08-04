f419f6208c01e633e3f60a6995ce5f63e4be9a36
Yemi Fakorede <yemifakorede@gmail.com>
2020-08-04 11:18:20 -0400

    squares now change colour when there is a winner

--- src/HelperFunctions.js
+++ src/HelperFunctions.js
@@ -13,6 +13,7 @@ export const getWinner = (board) => {
 
     for (let i = 0; i < winningLines.length; i++) {
         if (winningLines[i] === "XXX" || winningLines[i] === "OOO") {
+            changeSquareColour(getWinningSquaresArray(i))
             return winningLines[i][0] + " wins";
         }
     }
@@ -27,4 +28,31 @@ export const allSquaresOccupied = (board) => {
         }
     }
     return true;
+}
+
+export const getWinningSquaresArray = (winningLinesIndex) => {
+    if (winningLinesIndex === 0) {
+        return [0, 4, 8]
+    } else if (winningLinesIndex === 1) {
+        return [6, 4, 2]
+    } else if (winningLinesIndex === 2) {
+        return [0, 1, 2]
+    } else if (winningLinesIndex === 3) {
+        return [3, 4, 5]
+    } else if (winningLinesIndex === 4) {
+        return [6, 7, 8]
+    } else if (winningLinesIndex === 5) {
+        return [0, 3, 6]
+    } else if (winningLinesIndex === 6) {
+        return [1, 4, 7]
+    } else if (winningLinesIndex === 7) {
+        return [2, 5, 8]
+    }
+    return []
+}
+
+export const changeSquareColour = (winningArray) => {
+    winningArray.forEach((squareIndex) => {
+        document.getElementById("square-" + squareIndex).style.backgroundColor = "red"
+    })
 }
